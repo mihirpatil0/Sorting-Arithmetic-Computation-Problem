3ae7e5c4c8e709b45d2268f490f3c92d0bc46830
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 14:25:45 +0530

    Stored all the values inside Dictionary and printed the output with a key

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -2,14 +2,21 @@
 
 echo "Welcome to Arithmetic Computation & Sorting Problem"
 
+declare -A Dict
+
 read -p "Enter First Input in Number Format : " num1
 read -p "Enter second Input in Number Format : " num2
 read -p "Enter Third Input in Number Format : " num3
 
 echo -e "First Input : $num1\nSecond Input : $num2\nThird Input : $num3 "
 
-#AddandMulti=$(($num1+$num2*$num3))
-#AddandMulti=$(($num1*$num2+$num3))
-#AddandMulti=$(awk "BEGIN{print ($num3+$num1/$num2)}")
-AddandMulti=$(awk "BEGIN{print ($num1%$num2+$num3)}")
-echo "OutPut of Arithmetic Operation ( $num1%$num2+$num3 ) is : $AddandMulti"
+Dict[a+b*c]=$(($num1+$num2*$num3))
+Dict[a*b+c]=$(($num1*$num2+$num3))
+Dict[c+a/b]=$(awk "BEGIN{print ($num3+$num1/$num2)}")
+Dict[a%b+c]=$(awk "BEGIN{print ($num1%$num2+$num3)}")
+
+echo "OutPut of Arithmetic Operation Stored inside Dictionary is"
+for Key in ${!Dict[@]}
+do
+echo $Key = ${Dict[$Key]}
+done
