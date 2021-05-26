333321a2de7e5366e6c0f0c14a337ee825ce1abc
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 13:26:46 +0530

    Added user input code

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Welcome to Arithmetic Computation & Sorting Problem"
+
+read -p "Enter First Input in Number Format : " num1
+read -p "Enter second Input in Number Format : " num2
+read -p "Enter Third Input in Number Format : " num3
+
+echo -e "First Input : $num1\nSecond Input : $num2\nThird Input : $num3 "
